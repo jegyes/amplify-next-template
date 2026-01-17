e6195f7ef3b0b8c73b47f395bab2e5f6a6d6baed
jegyes <jegyes@madisoncollege.edu>
2026-01-17 13:30:51 -0600

    added authentication to page and layout; commented out

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+// import { useAuthenticator } from "@aws-amplify/ui-react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -13,6 +14,9 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
+    
+  // const { signOut } = useAuthenticator();
+
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -56,6 +60,8 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+
+      {/* <button onClick={signOut}>Sign out</button> */}
     </main>
   );
 }
