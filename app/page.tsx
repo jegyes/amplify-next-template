1dc3d2747e33a86881397c1147f640a085831a0f
jegyes <jegyes@madisoncollege.edu>
2026-01-17 13:59:11 -0600

    use client - changes added to enable auth box to work client side

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-// import { useAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -15,7 +15,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
     
-  // const { signOut } = useAuthenticator();
+  const { signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -61,7 +61,7 @@ export default function App() {
         </a>
       </div>
 
-      {/* <button onClick={signOut}>Sign out</button> */}
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
