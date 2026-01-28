6fbdfa5e57776df62fd0086b9adec66d52c8b0b3
jegyes <jegyes@madisoncollege.edu>
2026-01-27 18:31:38 -0600

    commented some basic imports into the components - nothing functional yet

---
+++ app/components/TempTrackerServer.tsx
@@ -0,0 +1,19 @@
+// import LocationFinderServer from "./LocationFinderServer";
+
+// export default async function TempTrackerServer() {
+
+//         // const response = await fetch('https://apip.cc/json');
+//         // const locationData = await response.json();
+//         // console.log(locationData);
+//         // const locationInfo =locationData;
+
+//         console.log(locationInfo?.Latitude);
+
+
+//     // return (
+//     //     <>
+//     //       <h1>Hello from {locationInfo?.City} - server component</h1>
+//     //     </>
+
+//     // )
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import "./../app/app.css";
 import ToDo from "./components/ToDo";
 import LocationFinderClient from "./components/LocationFinderClient";
 import LocationFinderServer from "./components/LocationFinderServer";
+// import TempTrackerClient from "./components/TempTrackerClient";
+// import TempTrackerServer from "./components/TempTrackerServer";
 
 
 export default function App() {
@@ -10,7 +12,9 @@ export default function App() {
   return (
     <main>
       <LocationFinderClient />
+      {/* <TempTrackerClient /> */}
       <LocationFinderServer />
+      {/* <TempTrackerServer /> */}
       <ToDo />
     </main>
   );
